fae70a9b0eb6171be107ce7b2efbbb08578ad1f2
RDF-online-document <dnsanzabaganwa@gmail.com>
2024-12-24 12:48:48 +0200

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const passwordInput = document.getElementById('password');
-    const togglePassword = document.querySelector('.toggle-password');
-
-    togglePassword.addEventListener('click', () => {
-        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-        passwordInput.setAttribute('type', type);
-    });
-});
